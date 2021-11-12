86e245306c063583701f0273745faf61659955b0
Hannah <kyki73394@gmail.com>
2021-11-12 16:46:02 -0500

    toggle works? for single ID only

--- main.js
+++ main.js
@@ -3,12 +3,33 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
- document.getElementById("allnav").style.color = "black";
+ document.getElementById("nav_proj").style.color = "black";
+ document.getElementById("nav_extra").style.color = "black";
+ document.getElementById("nav_abt").style.color = "black";
+ document.getElementById("nav_logo").style.color = "black";
 
 } else if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-  document.getElementById("allnav").style.color = "black";
+  document.getElementById("nav_proj").style.color = "black";
+  document.getElementById("nav_extra").style.color = "black";
+  document.getElementById("nav_abt").style.color = "black";
+  document.getElementById("nav_logo").style.color = "black";
 
 } else {
-    document.getElementById("allnav").style.color = "white";
+    document.getElementById("nav_proj").style.color = "white";
+    document.getElementById("nav_extra").style.color = "white";
+    document.getElementById("nav_abt").style.color = "white";
+    document.getElementById("nav_logo").style.color = "white";
+
   }
+}
+
+//edited from toggle Dark Mode from w3 schools
+function toggleUX() {
+  var elem = document.getElementById("box_ux");
+  elem.classList.toggle("box_off");
+}
+
+function toggleCode() {
+  var elem = document.getElementById("box_code");
+  elem.classList.toggle("box_off");
 }
