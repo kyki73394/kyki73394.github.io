5885020ea057bf69671da14b237d55e745eead89
Hannah <kyki73394@gmail.com>
2021-12-16 15:26:43 -0500

    re-designed the home page

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
- document.getElementById("nav_proj").style.color = "black";
- document.getElementById("nav_extra").style.color = "black";
- document.getElementById("nav_abt").style.color = "black";
+ // document.getElementById("nav_proj").style.color = "black";
+ // document.getElementById("nav_extra").style.color = "black";
+ // document.getElementById("nav_abt").style.color = "black";
  document.getElementById("nav_logo").style.color = "black";
 
 } else if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-  document.getElementById("nav_proj").style.color = "black";
-  document.getElementById("nav_extra").style.color = "black";
-  document.getElementById("nav_abt").style.color = "black";
+  // document.getElementById("nav_proj").style.color = "black";
+  // document.getElementById("nav_extra").style.color = "black";
+  // document.getElementById("nav_abt").style.color = "black";
   document.getElementById("nav_logo").style.color = "black";
 
 } else {
-    document.getElementById("nav_proj").style.color = "white";
-    document.getElementById("nav_extra").style.color = "white";
-    document.getElementById("nav_abt").style.color = "white";
+    // document.getElementById("nav_proj").style.color = "white";
+    // document.getElementById("nav_extra").style.color = "white";
+    // document.getElementById("nav_abt").style.color = "white";
     document.getElementById("nav_logo").style.color = "white";
 
   }
