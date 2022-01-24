d1b6de5f277c4a2542c2d6760742573842a17faa
Hannah <kyki73394@gmail.com>
2022-01-23 20:31:42 -0500

    changed landing page to match older sky iteration

--- main.js
+++ main.js
@@ -2,24 +2,14 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
- // document.getElementById("nav_proj").style.color = "black";
- // document.getElementById("nav_extra").style.color = "black";
- // document.getElementById("nav_abt").style.color = "black";
- document.getElementById("nav_logo").style.color = "black";
+if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+  document.getElementById("nav_logo").style.color = "black";
 
 } else if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-  // document.getElementById("nav_proj").style.color = "black";
-  // document.getElementById("nav_extra").style.color = "black";
-  // document.getElementById("nav_abt").style.color = "black";
   document.getElementById("nav_logo").style.color = "black";
 
 } else {
-    // document.getElementById("nav_proj").style.color = "white";
-    // document.getElementById("nav_extra").style.color = "white";
-    // document.getElementById("nav_abt").style.color = "white";
-    document.getElementById("nav_logo").style.color = "white";
-
+  document.getElementById("nav_logo").style.color = "white";
   }
 }
 
